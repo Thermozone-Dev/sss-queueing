a77492e3b033177c9259ce5e9e5ba40187585711
joshraphaelorolfo <johnclark852000@gmail.com>
2025-08-17 15:29:12 +0800

    Transaction steps

---
+++ app/Models/TransactionStep.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class TransactionStep
+ * 
+ * @property int $id
+ * @property int $transaction_id
+ * @property string $title
+ * @property string|null $description
+ * @property bool $is_required
+ * @property int $sort_order
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * 
+ * @property Transaction $transaction
+ *
+ * @package App\Models
+ */
+class TransactionStep extends Model
+{
+	protected $table = 'transaction_steps';
+
+	protected $casts = [
+		'transaction_id' => 'int',
+		'is_required' => 'bool',
+		'sort_order' => 'int'
+	];
+
+	protected $fillable = [
+		'transaction_id',
+		'title',
+		'description',
+		'is_required',
+		'sort_order'
+	];
+
+	public function transaction()
+	{
+		return $this->belongsTo(Transaction::class);
+	}
+}
