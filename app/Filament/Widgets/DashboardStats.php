cd856c86a74f3ad4fec18ba502c2058f0f8e186e
joshraphaelorolfo <johnclark852000@gmail.com>
2025-08-17 15:45:48 +0800

    Dashboard widgets, logo and color

---
+++ app/Filament/Widgets/DashboardStats.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Station;
+use Carbon\Carbon;
+use Filament\Forms\Components\Section;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class DashboardStats extends BaseWidget
+{
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('', '1')
+                ->color('warning')
+                ->icon('heroicon-o-users')
+                ->description('Total Clients Today'),
+            Stat::make('', Carbon::now()->format('i').'min')
+                ->color('info')
+                ->icon('heroicon-o-clock')
+                ->description('Average Wait Time'),
+            Stat::make('Manage Stations', Station::where('status', 1)->count())
+                ->color('success')
+                ->icon('heroicon-o-play')
+                ->description('Active Stations')
+                ->url('admin/stations'),
+            Stat::make('Manage transactions', '1')
+                ->color('danger')
+                ->icon('heroicon-o-exclamation-triangle')
+                ->description('Ongoing Transactions')
+                ->url('admin/transactions')
+        ];
+    }
+}
