9e71d63a8906354f7f4f4acb9ab6a629924e9b0c
joshraphaelorolfo <johnclark852000@gmail.com>
2025-08-19 04:09:39 +0800

    Update DashboardStats.php

--- app/Filament/Widgets/DashboardStats.php
+++ app/Filament/Widgets/DashboardStats.php
@@ -10,33 +10,50 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class DashboardStats extends BaseWidget
 {
+    protected static ?int $sort = 1;
 
+    protected static ?string $pollingInterval = null;
+    public $station;
+    public $activeCount;
+    public $pendingCount;
+    public $processingCount;
+    public $total;
+
+    public function mount(){
+        if (auth()->user()->hasRole('staff')) {
+            $station = auth()->user()->stations()->first();
+
+            $this->activeCount = $station->activeQueues->count(); // Count active queues
+            $this->pendingCount = $station->pendingQueues->count(); // Count pending queues
+            $this->processingCount = $station->processingQueues->count(); // Count processing queues
+        } else{
+            $stations = Station::all();
+
+            $this->activeCount = $stations->sum(fn ($station) => $station->queues()->active()->count());
+            $this->pendingCount = $stations->sum(fn ($station) => $station->queues()->active()->where('status_id', 1)->count());
+            $this->processingCount = $stations->sum(fn ($station) => $station->queues()->active()->where('status_id', 2)->count());
+        }
+        $this->total = $this->activeCount + $this->pendingCount + $this->processingCount;
+    }
     protected function getStats(): array
     {
         return [
-            Stat::make('', '1')
+            Stat::make('', $this->total)
                 ->color('warning')
                 ->icon('heroicon-o-users')
                 ->description('Total Clients Today'),
-            Stat::make('', Carbon::now()->format('i').'min')
-                ->color('info')
-                ->icon('heroicon-o-clock')
-                ->description('Average Wait Time'),
-            Stat::make('Manage Stations', Station::where('status', 1)->count())
+            Stat::make('', $this->activeCount)
                 ->color('success')
                 ->icon('heroicon-o-play')
-                ->description('Active Stations')
-                ->url('admin/stations'),
-            Stat::make('Manage transactions', '1')
+                ->description('Active Queues'),
+            Stat::make('', $this->processingCount)
+                ->color('info')
+                ->icon('heroicon-o-clock')
+                ->description('Processing'),
+            Stat::make('', $this->pendingCount)
                 ->color('danger')
                 ->icon('heroicon-o-exclamation-triangle')
-                ->description('Ongoing Transactions')
-                ->url('admin/transactions')
+                ->description('Pending')
         ];
-    }
-
-    public static function canView(): bool
-    {
-        return !auth()->user()?->hasRole('staff');
     }
 }
