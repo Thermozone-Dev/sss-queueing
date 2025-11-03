7d0ebb22d8f0e80a086a1fe28a0a772e3ecb0e9f
thirdy0990 <thirdymaturan16@gmail.com>
2025-11-03 10:38:24 +0800

    Add soft delete to transactions table

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -9,6 +9,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Transaction
@@ -28,6 +29,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Transaction extends Model
 {
+    use SoftDeletes;
 	protected $table = 'transactions';
 
 	protected $casts = [
